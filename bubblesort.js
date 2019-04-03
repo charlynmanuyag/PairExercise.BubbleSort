44815e7eafca660ed73c312d3d34632a955f8218
Charlyn Manuyag <charlyn.manuyag@berkeley.edu>
2019-04-03 12:32:01 -0400

    added and passed with swap function

--- bubblesort.js
+++ bubblesort.js
@@ -4,11 +4,16 @@ function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        // let temp = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = temp;
+        [arr[j], arr[j + 1]] = swap(arr[j], arr[j + 1]);
       }
     }
   }
   return arr;
+}
+
+function swap(a, b) {
+  return [b, a];
 }
